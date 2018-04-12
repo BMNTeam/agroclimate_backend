eed4dde719eed816ea3ecc917c49fbfe167545bd
BMNTeam <maksim_bender08@mail.ru>
2018-04-12 21:59:27 +0300

    Fixed separate by decades function

--- admin/actions/classes/Gtk.php
+++ admin/actions/classes/Gtk.php
@@ -1,12 +1,13 @@
 <?php
 require_once('../../../include/DB_itit.php');
 require_once('Helpers.php');
+
 /**
  *
  *
  */
-
-class Gtk{
+class Gtk
+{
     private $db;
     private $table_name = 'ClimateData_GTK';
     private $helpers;
@@ -25,7 +26,7 @@ class Gtk{
     public function set($post)
     {
         $year = $post['startYear'];
-        if(! $this->year_exists($year)) $this->insert_empty_year($year);
+        if (!$this->year_exists($year)) $this->insert_empty_year($year);
 
 
     }
@@ -38,15 +39,14 @@ class Gtk{
          * @param $post | [
          * @return array grouped by months | [[0] => [P4_1 => 10 .. T4_3 = 22] [1] ...]
          */
-        function filter_months($post){
+        function filter_months($post)
+        {
             $months = array();
-            for($m = 4; $m <= 10; $m++)
-            {
+            for ($m = 4; $m <= 10; $m++) {
                 $month = array();
-                foreach ($post as $key=>$val)
-                {
+                foreach ($post as $key => $val) {
                     //Check if key starts with P or T and has index equal to permitted
-                    if(preg_match("/(P|T)$m.+/", $key) ) $month += [$key => $val];
+                    if (preg_match("/(P|T)$m.+/", $key)) $month += [$key => $val];
                 }
                 array_push($months, $month);
                 $month = null;
@@ -64,9 +64,9 @@ class Gtk{
         function get_days_in_month($month, $year)
         {
             $days_in_months = null;
-            foreach ($month as $key => $value){
+            foreach ($month as $key => $value) {
                 //Find number in given key | P1_3 find 1 => P('1')_3
-                if(preg_match('/P(\d?)_.+/', $key, $month_number)){
+                if (preg_match('/P(\d+)_.+/', $key, $month_number)) {
                     $month_number = (int)trim($month_number[1]);
                     $days_in_months = cal_days_in_month(CAL_GREGORIAN, $month_number, $year);
                     break;
@@ -76,39 +76,47 @@ class Gtk{
             return $days_in_months;
         }
 
-
+        /**
+         * @param $months array permitted months | ex: [[0] => [P4_1 => 10 .. T4_3 = 22] [1] ...]
+         * @param $year
+         * @return array | example: [ [0] => [ ['P1_1'] => 10; ['T1_1'] => 4; ['days'] => 10 ] ... [6] =>.. ]
+         */
         function separate_by_decades($months, $year)
         {
             $months_by_decades = [];
-            $count = 0;
-            foreach ($months as $month => $key)
-            {
 
-                //TODO: Fix for unknown reason days counts only on the last day
+            foreach ($months as $month => $key) { // $key example [P1_1] => 10
+
                 $days = (int)get_days_in_month($key, $year);
 
-                $first_decade = array_slice($key, 0, 2);
+                $first_decade = array_slice($key, 0, 2); // Ex:[ ['P1_1' => 10] ['T1_1']=>4 ]
                 $second_decade = array_slice($key, 2, 2);
-                $third_decade = array_slice($key, 4,2);
+                $third_decade = array_slice($key, 4, 2);
 
-                foreach ([$first_decade, $second_decade, $third_decade] as $key => $val)
-                {
-                    //print_r($days -10);
-                    if($days <= 13) {
+                $month = [];
+
+                foreach ([$first_decade, $second_decade, $third_decade] as $key => $val) {
+                    if ($days >= 13) {
                         $val['days'] = 10;
                         $days -= 10;
 
                     } else {
                         $val['days'] = $days;
                     }
-                    //print_r($val);
+                    array_push($month, $val); // $month = [ ['P1_1'] => 10; ['T1_1'] => 4; ['days'] => 10; ]
                 }
+                array_push($months_by_decades, $month);
+                // $month_by_decades = [ [0] => [ ['P1_1'] => 10; ['T1_1'] => 4; ['days'] => 10 ]... [6].. ]
             }
+
+            return $months_by_decades;
         }
 
         $permitted_months = filter_months($post);
 
-        separate_by_decades($permitted_months, 2018);
+        $permitted_months = separate_by_decades($permitted_months);
+
+        print_r($permitted_months);
 
 
     }
@@ -132,22 +140,22 @@ class Gtk{
     private function generate_columns()
     {
         $columns = null;
-        for($p = 4; $p <= 10; $p++) //Months
+        for ($p = 4; $p <= 10; $p++) //Months
         {
-            foreach (['P', 'T', 'D'] as $key)
-            {
+            foreach (['P', 'T', 'D'] as $key) {
                 $columns .= "$key$p,";
             }
         }
         return rtrim($columns, ',');
 
     }
+
     private function year_exists($year)
     {
         $is_exist = true;
         $query = "SELECT * FROM $this->table_name WHERE Year = $year";
         $result = $this->db->query($query);
-        if(empty($result->fetchAll())) {
+        if (empty($result->fetchAll())) {
             $is_exist = false;
         }
         return $is_exist;
